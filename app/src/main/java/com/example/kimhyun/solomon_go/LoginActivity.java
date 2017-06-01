0a1bd4a0bd21e777aaffb8e536fd757cea882c9f
김현 <jun123101@gmail.com>
2017-06-02 01:31:33 +0900

    로그인 구현 완

--- app/src/main/java/com/example/kimhyun/solomon_go/LoginActivity.java
+++ app/src/main/java/com/example/kimhyun/solomon_go/LoginActivity.java
@@ -19,10 +19,8 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLConnection;
 import java.net.URLEncoder;
@@ -62,6 +60,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
 
         if(auto_login.getBoolean("Auto_Login_enabled", false)){
+
             et_id.setText(auto_login.getString("ID", ""));
             et_password.setText(auto_login.getString("PW", ""));
             ckbox_autologin.setChecked(true);
@@ -71,11 +70,6 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
             String password = et_password.getText().toString();
 
             insertToDatabase(id, password);
-
-
-            GetData task = new GetData();
-            task.execute("http://jun123101.cafe24.com/login.php");
-            Log.d("Login", "onclick까지 먹음");
 
 
 
@@ -120,11 +114,6 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
                 insertToDatabase(id, password);
 
 
-                GetData task = new GetData();
-                task.execute("http://jun123101.cafe24.com/login.php");
-                Log.d("Login", "onclick까지 먹음");
-
-
                 break;
 
             case R.id.btn_register:
@@ -150,21 +139,58 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
             }
 
             @Override
-            protected void onPostExecute(String s) {
-                super.onPostExecute(s);
+            protected void onPostExecute(String result) {
+                super.onPostExecute(result);
 
                 loading.dismiss();
-                Toast.makeText(getApplicationContext(), s, Toast.LENGTH_LONG).show();
+
+                try {
+
+                    Log.d("Login", result);
+
+                    JSONObject jsonObj = new JSONObject(result);
+
+                    String name = jsonObj.getJSONArray("result").getJSONObject(0).getString("name");
+
+                    if(name != "null"){       //로그인이 성공한다면
+
+                        Toast.makeText(getApplicationContext(), name + "님 안녕하세요", Toast.LENGTH_SHORT).show();
+
+                        auto_login = getSharedPreferences("auto", Activity.MODE_PRIVATE);
+                        SharedPreferences.Editor editer = auto_login.edit();
+
+                        editer.putString("id", et_id.getText().toString());
+                        editer.putString("password", et_password.getText().toString());
+                        editer.commit();
+
+
+
+                        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                        intent.addFlags(intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+                        startActivity(intent);
+
+                        finish();
+
+                    }
+                    else{
+                        Toast.makeText(getApplicationContext(), "로그인 실패! ㅠㅠ ", Toast.LENGTH_SHORT).show();
+                    }
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
             }
 
             @Override
             protected String doInBackground(String... params) {
+                String link = "http://jun123101.cafe24.com/login.php";
 
                 try {
                     String id =  params[0];
                     String password = params[1];
 
-                    String link = "http://jun123101.cafe24.com/login.php";
                     String data = URLEncoder.encode("id", "UTF-8") + "="
                             + URLEncoder.encode(id, "UTF-8");
                     data += "&" + URLEncoder.encode("password", "UTF-8") + "="
@@ -193,11 +219,13 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
                         sb.append(line);
                         break;
                     }
+                    Log.d("sbbb", sb.toString());
                     return sb.toString();
                 } catch (Exception e) {
 
                     return new String("Exception: " + e.getMessage());
                 }
+
 
             }
         }
@@ -206,115 +234,5 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         task.execute(id, password);
     }
 
-    private class GetData extends AsyncTask<String, Void, String> {
-        ProgressDialog progressDialog;
-
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-
-            progressDialog = ProgressDialog.show(LoginActivity.this,
-                    "Please Wait", null, true, true);
-        }
-
-
-        @Override
-        protected void onPostExecute(String result) {
-            super.onPostExecute(result);
-
-            progressDialog.dismiss();
-            Log.d("Login", "PostExecute까지 먹음");
-            Log.d("result", result);
-
-
-            try {
-
-                JSONObject jsonObj = new JSONObject(result);
-                Log.d("Login", "Json받자");
-                String name = jsonObj.getJSONArray("result").getJSONObject(0).getString("name");
-
-                Log.d("Login", "Json받았니??? ㅠㅠㅠㅠ ");
-                Toast.makeText(getApplicationContext(), name + "님 ㅎㅇ", Toast.LENGTH_SHORT).show();
-
-                if(name != null){       //로그인이 성공한다면
-
-                    auto_login = getSharedPreferences("auto", Activity.MODE_PRIVATE);
-                    SharedPreferences.Editor editer = auto_login.edit();
-
-                    editer.putString("id", et_id.getText().toString());
-                    editer.putString("password", et_password.getText().toString());
-                    editer.commit();
-
-
-
-                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                    intent.addFlags(intent.FLAG_ACTIVITY_CLEAR_TOP);
-
-                    startActivity(intent);
-
-                    finish();
-
-                }
-
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
-
-        }
-
-
-        @Override
-        protected String doInBackground(String... params) {
-
-            String serverURL = params[0];
-
-
-            try {
-
-                URL url = new URL(serverURL);
-                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
-
-
-                httpURLConnection.setReadTimeout(5000);
-                httpURLConnection.setConnectTimeout(5000);
-                httpURLConnection.connect();
-
-
-                int responseStatusCode = httpURLConnection.getResponseCode();
-
-                InputStream inputStream;
-                if (responseStatusCode == HttpURLConnection.HTTP_OK) {
-                    inputStream = httpURLConnection.getInputStream();
-                } else {
-                    inputStream = httpURLConnection.getErrorStream();
-                }
-
-
-                InputStreamReader inputStreamReader = new InputStreamReader(inputStream, "UTF-8");
-                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
-
-                StringBuilder sb = new StringBuilder();
-                String line;
-
-                while ((line = bufferedReader.readLine()) != null) {
-                    sb.append(line);
-                }
-
-
-                bufferedReader.close();
-
-
-                return sb.toString().trim();
-
-
-            } catch (Exception e) {
-
-                Toast.makeText(getApplicationContext(), "Error", Toast.LENGTH_SHORT).show();
-                return null;
-            }
-
-        }
-
-    }
 
 }
