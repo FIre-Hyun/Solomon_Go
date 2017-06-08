7a50170c60bef7adf46becaacfda1b0af0a7a2f4
LEDPEAR <https://github.com/ledpear>
2017-06-08 09:50:36 +0900

    근처 솔로몬 gps값 추가

--- app/src/main/java/com/example/kimhyun/solomon_go/NearsolomonActivity.java
+++ app/src/main/java/com/example/kimhyun/solomon_go/NearsolomonActivity.java
@@ -28,6 +28,39 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.concurrent.ExecutionException;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CircleOptions;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
+
+
 public class NearsolomonActivity extends AppCompatActivity {
     private static String TAG = "Nearsolomon";
 
@@ -49,6 +82,8 @@ public class NearsolomonActivity extends AppCompatActivity {
 
     Drawable drawable;
 
+    String id;
+    SharedPreferences sp_id;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -257,5 +292,133 @@ public class NearsolomonActivity extends AppCompatActivity {
 
         return bitmap;
 
+    }
+    //여기서부터 gps부분
+
+    private void starLocationService(){
+        LocationManager manager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+        GPSListener gpsListener = new GPSListener();
+
+        long minTime = 1000;//GPS정보 전달 시간 지정 - 1초마다 위치정보 전달
+        float minDistance = 0;//이동거리 지정 - 이동하면 무조건 갱신
+
+        //showCurrentLocation((double)latitude_position,(double)longitude_position);//위치이동
+        //위치정보는 위치 프로바이더(Location Provider)를 통해 얻는다
+        try {
+            // GPS를 이용한 위치 요청
+            manager.requestLocationUpdates(
+                    LocationManager.GPS_PROVIDER, //위치 정보 확인 방법 설정
+                    minTime, // 위치 정보 갱신 시간 설정
+                    minDistance, //위치 정보 갱신을 위한 최소 이동거리 설정
+                    gpsListener);//위치가 변동될 때마다 위치 정보 갱신을 위한 리스너 설정
+
+            // 네트워크(기지국)를 이용한 위치 요청
+            manager.requestLocationUpdates(
+                    LocationManager.NETWORK_PROVIDER,
+                    minTime,
+                    minDistance,
+                    gpsListener);
+
+            // 위치 확인이 안되는 경우에도 최근에 확인된 위치 정보 먼저 확인
+            Location lastLocation = manager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            if (lastLocation != null) {
+                Double latitude = lastLocation.getLatitude();
+                Double longitude = lastLocation.getLongitude();
+                //Toast.makeText(getApplicationContext(), "Last Known Location : " + "Latitude : "
+                //        + latitude + "\nLongitude:" + longitude, Toast.LENGTH_LONG).show();
+                LatLng nowPoint = new LatLng(latitude,longitude);
+            }
+
+        } catch(SecurityException ex) {
+            ex.printStackTrace();
+        }
+    }
+    private class GPSListener implements LocationListener {
+        //위치 정보가 확인(수신)될 때마다 자동 호출되는 메소드
+        public void onLocationChanged(Location location) {
+
+            Double latitude = location.getLatitude();
+            Double longitude = location.getLongitude();
+            if(location.getAccuracy()<1000){
+
+                insertToDatabase(latitude.toString(), longitude.toString());
+            }
+
+        }
+
+        public void onProviderDisabled(String provider) {
+        }
+
+        public void onProviderEnabled(String provider) {
+        }
+
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+        }
+    }// end of GPSListener
+
+    private void insertToDatabase(String latitude, String longitude) {
+
+        class InsertData extends AsyncTask<String, Void, String> {
+
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+
+            }
+
+            @Override
+            protected void onPostExecute(String s) {
+
+                super.onPostExecute(s);
+
+            }
+
+            @Override
+            protected String doInBackground(String... params) {
+
+                try {
+                    String latitude = (String) params[0];
+                    String longitude = (String) params[1];
+
+                    String link = "http://jun123101.cafe24.com/insertgps.php";
+                    String data = URLEncoder.encode("latitude", "UTF-8") + "="
+                            + URLEncoder.encode(latitude, "UTF-8");
+                    data += "&" + URLEncoder.encode("longitude", "UTF-8") + "="
+                            + URLEncoder.encode(longitude, "UTF-8");
+                    data += "&" + URLEncoder.encode("id", "UTF-8") + "="
+                            + URLEncoder.encode(id, "UTF-8");
+
+                    URL url = new URL(link);
+                    URLConnection conn = url.openConnection();
+
+                    conn.setDoOutput(true);
+                    OutputStreamWriter wr =
+                            new OutputStreamWriter(conn.getOutputStream());
+
+                    wr.write(data);
+                    wr.flush();
+
+                    BufferedReader reader = new BufferedReader(
+                            new InputStreamReader(conn.getInputStream()));
+
+                    StringBuilder sb = new StringBuilder();
+                    String line = null;
+
+                    // Read Server Response
+                    while ((line = reader.readLine()) != null) {
+                        sb.append(line);
+                        break;
+                    }
+                    return sb.toString();
+                } catch (Exception e) {
+
+                    return new String("Exception: " + e.getMessage());
+                }
+
+            }
+        }
+
+        InsertData task = new InsertData();
+        task.execute(latitude, longitude);
     }
 }
