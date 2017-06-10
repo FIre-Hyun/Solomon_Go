267504af388e7a4f8f6cd783f6e78ed4f95edb1b
김현 <jun123101@gmail.com>
2017-06-10 18:45:15 +0900

    회원가입-이미지 업로드 구

--- app/src/main/java/com/example/kimhyun/solomon_go/LoginRegisterActivity.java
+++ app/src/main/java/com/example/kimhyun/solomon_go/LoginRegisterActivity.java
@@ -1,15 +1,15 @@
 package com.example.kimhyun.solomon_go;
 
 import android.app.ProgressDialog;
-import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
+import android.util.Base64;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -20,8 +20,14 @@ import android.widget.Spinner;
 import android.widget.Toast;
 
 import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.net.URLEncoder;
@@ -37,6 +43,10 @@ public class LoginRegisterActivity extends AppCompatActivity {
     ImageView imageView_picture;
 
     RadioButton rb_man, rb_girl;
+
+    Bitmap imageBitmap;
+
+    private int PICK_IMAGE_REQUEST = 1;
 
     SharedPreferences auto_login;
     SharedPreferences.Editor editor;
@@ -60,8 +70,8 @@ public class LoginRegisterActivity extends AppCompatActivity {
 
         spin_home = (Spinner) findViewById(R.id.spinner_home);
         spin_year = (Spinner) findViewById(R.id.spinner_year);
-        spin_month = (Spinner) findViewById(R.id.spinner_month);
-        spin_day = (Spinner) findViewById(R.id.spinner_day);
+//        spin_month = (Spinner) findViewById(R.id.spinner_month);
+//        spin_day = (Spinner) findViewById(R.id.spinner_day);
 
         rb_man = (RadioButton) findViewById(R.id.rb_man);
         rb_girl = (RadioButton) findViewById(R.id.rb_girl);
@@ -77,21 +87,21 @@ public class LoginRegisterActivity extends AppCompatActivity {
         adapter.setDropDownViewResource(
                 android.R.layout.simple_spinner_dropdown_item
         );
-        ArrayAdapter adapter_month = ArrayAdapter.createFromResource(
-                this, R.array.birth_month, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(
-                android.R.layout.simple_spinner_dropdown_item
-        );
-
-        ArrayAdapter adapter_day = ArrayAdapter.createFromResource(
-                this, R.array.birth_day, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(
-                android.R.layout.simple_spinner_dropdown_item
-        );
+//        ArrayAdapter adapter_month = ArrayAdapter.createFromResource(
+//                this, R.array.birth_month, android.R.layout.simple_spinner_item);
+//        adapter.setDropDownViewResource(
+//                android.R.layout.simple_spinner_dropdown_item
+//        );
+//
+//        ArrayAdapter adapter_day = ArrayAdapter.createFromResource(
+//                this, R.array.birth_day, android.R.layout.simple_spinner_item);
+//        adapter.setDropDownViewResource(
+//                android.R.layout.simple_spinner_dropdown_item
+//        );
         spin_home.setAdapter(adapter);
         spin_year.setAdapter(adapter_year);
-        spin_month.setAdapter(adapter_month);
-        spin_day.setAdapter(adapter_day);
+//        spin_month.setAdapter(adapter_month);
+//        spin_day.setAdapter(adapter_day);
 
 
         btn_register.setOnClickListener(new View.OnClickListener() {
@@ -125,7 +135,15 @@ public class LoginRegisterActivity extends AppCompatActivity {
                     else
                         sex = "1";
 
-                    insertToDatabase(id, password, name, hobby, type, job, home, sex, age);
+                    insertToDatabase(id, password, name, hobby, type, job, home, sex, age);     //여기까지가 회원가입 정보 db에 insert
+
+                    ByteArrayOutputStream stream = new ByteArrayOutputStream(); // 여기부터가 사진 업로드
+                    imageBitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream);
+
+                    byte[] byte_arr = stream.toByteArray();
+                    String encodedString = Base64.encodeToString(byte_arr, 0);
+
+                    upload("http://jun123101.cafe24.com/ImageUpload.php","POST",getEncoded64ImageStringFromBitmap(imageBitmap), id);
 
                     Toast.makeText(getApplicationContext(), "회원가입이 완료되었습니다.", Toast.LENGTH_SHORT).show();
                     Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
@@ -140,27 +158,35 @@ public class LoginRegisterActivity extends AppCompatActivity {
         btn_gallery.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-//                Intent intent = new Intent();
-//                // Gallery 호출
-//                intent.setType("image/*");
-//                intent.setAction(Intent.ACTION_GET_CONTENT);
 
-                Intent intent = new Intent(Intent.ACTION_PICK);
-                intent.setType(MediaStore.Images.Media.CONTENT_TYPE);
-                // 잘라내기 셋팅
-                intent.putExtra("crop", "true");
-                intent.putExtra("aspectX", 0);
-                intent.putExtra("aspectY", 0);
-                intent.putExtra("outputX", 200);
-                intent.putExtra("outputY", 150);
-                try {
-                    intent.putExtra("return-data", true);
-                    startActivityForResult(Intent.createChooser(intent,
-                            "Complete action using"), 2);   // PICK FROM GALLERY
-                } catch (ActivityNotFoundException e) {
-                    // Do nothing for now
+//                Intent intent = new Intent(Intent.ACTION_PICK);
+//                intent.setType(MediaStore.Images.Media.CONTENT_TYPE);
+//                // 잘라내기 셋팅
+//                intent.putExtra("crop", "true");
+//                intent.putExtra("aspectX", 0);
+//                intent.putExtra("aspectY", 0);
+//                intent.putExtra("outputX", 200);
+//                intent.putExtra("outputY", 150);
+//                try {
+//                    intent.putExtra("return-data", true);
+//                    startActivityForResult(Intent.createChooser(intent,
+//                            "Complete action using"), 2);   // PICK FROM GALLERY
+//                } catch (ActivityNotFoundException e) {
+//                    // Do nothing for now
+//                }
+//
+
+                Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+                    // startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
                 }
+                Intent intent = new Intent();
+                // Show only images, no videos or anything else
+                intent.setType("image/*");
 
+                intent.setAction(Intent.ACTION_PICK);
+                // Always show the chooser (if there are multiple options available)
+                startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST);
 
             }
         });
@@ -170,14 +196,17 @@ public class LoginRegisterActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == 2) {     //pick from gallery
-            Bundle extras2 = data.getExtras();
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
 
-            if (extras2 != null) {
-                Bitmap photo = extras2.getParcelable("data");
-                Log.d("register_photo", photo.toString());
-                imageView_picture.setImageBitmap(photo);
+            Uri uri = data.getData();
 
+            try {
+                imageBitmap= MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
+                // Log.d(TAG, String.valueOf(bitmap));
+
+                imageView_picture.setImageBitmap(imageBitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
             }
         }
     }
@@ -201,7 +230,7 @@ public class LoginRegisterActivity extends AppCompatActivity {
                 super.onPostExecute(s);
 
                 loading.dismiss();
-                Toast.makeText(getApplicationContext(), s, Toast.LENGTH_LONG).show();
+                //Toast.makeText(getApplicationContext(), s, Toast.LENGTH_LONG).show();
             }
 
             @Override
@@ -271,4 +300,73 @@ public class LoginRegisterActivity extends AppCompatActivity {
         InsertData task = new InsertData();
         task.execute(id, password, name, hobby, type, job, home, sex, age);
     }
+
+    public String getEncoded64ImageStringFromBitmap(Bitmap bitmap) {
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
+        byte[] byteFormat = stream.toByteArray();
+        String imgString = Base64.encodeToString(byteFormat, Base64.NO_WRAP);
+        return imgString;
+    }
+    public void upload(String Url, String method, String imageString, String id) {
+        new AsyncTask<String, String, String>() {
+            String method, imageString;
+            int tmp;
+            String data="";
+
+            protected void onPreExecute() {
+            }
+            @Override
+            protected String doInBackground(String... params) {
+                try {
+                    URL url = new URL(params[0]);
+                    method = params[1];
+                    String urlParams = params[2];
+                    String id_data = params[3];
+
+                    String id = URLEncoder.encode("id", "UTF-8") + "="
+                            + URLEncoder.encode(id_data, "UTF-8");
+
+                    HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+
+                    httpURLConnection.setDoOutput(true);
+                    httpURLConnection.setRequestMethod(method);
+
+                    httpURLConnection.setUseCaches(false);
+                    httpURLConnection.setAllowUserInteraction(false);
+
+                    OutputStream os = httpURLConnection.getOutputStream();
+
+                    String full_data = urlParams + "&" + id;
+
+                    os.write(full_data.getBytes());
+                    os.flush();
+                    os.close();
+
+                    InputStream is = httpURLConnection.getInputStream();
+                    while((tmp=is.read())!=-1){
+                        data+= (char)tmp;
+                    }
+
+                    is.close();
+                    httpURLConnection.disconnect();
+
+                    return data;
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                    return "Exception: "+e.getMessage();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    return "Exception: "+e.getMessage();
+                }
+            }
+
+            @Override
+            protected void onPostExecute(String msg) {
+//                Toast.makeText(MainActivity.this.getApplicationContext(),msg,Toast.LENGTH_LONG).show();
+            }
+        }.execute(Url, method, imageString, id);
+    }
+
+
 }
